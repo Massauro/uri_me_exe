a069ac50be282eadb65c005c703c57f99faa0b89
Massaru <massaruhagiopro@gmail.com>
2021-06-02 00:34:12 +0900

    update uri

--- 1000.py
+++ 1000.py
@@ -9,7 +9,7 @@ Entrada
 Este problema não possui nenhuma entrada.
 
 Saída
-Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o 
+Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o
 exemplo abaixo.
 """
 print("Hello Word!")
